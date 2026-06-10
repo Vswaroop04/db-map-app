ea9ad1960caa138c52ab78c7cf523af79419c6fb
vswaroop04 <vswaroop04@gmail.com>
2026-06-10 23:31:15 +0530

    exclude seed and api-tests folders from metro and watchman

--- metro.config.js
+++ metro.config.js
@@ -2,6 +2,8 @@ const path = require('path');
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
 
+const esc = (s) => s.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+
 const config = getDefaultConfig(__dirname);
 
 config.resolver.extraNodeModules = {
@@ -12,11 +14,12 @@ config.resolver.extraNodeModules = {
 };
 
 const nativewindModules = path.resolve(__dirname, 'node_modules/nativewind/node_modules');
-const esc = (s) => s.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
 
 config.resolver.blockList = [
+  // force nativewind to use root react and react-native instead of its bundled copies
   new RegExp(`^${esc(path.join(nativewindModules, 'react-native'))}(/.*)?$`),
   new RegExp(`^${esc(path.join(nativewindModules, 'react'))}(/.*)?$`),
+  // exclude standalone script folders so NativeWind doesn't look for tailwind.config there
   new RegExp(`^${esc(path.resolve(__dirname, 'seed'))}(/.*)?$`),
   new RegExp(`^${esc(path.resolve(__dirname, 'seed-script'))}(/.*)?$`),
   new RegExp(`^${esc(path.resolve(__dirname, 'api-tests'))}(/.*)?$`),
