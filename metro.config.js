ac4a27c7a3ecba4c117045247738616e143e009b
vswaroop04 <vswaroop04@gmail.com>
2026-06-10 16:21:50 +0530

    fix metro config to resolve nativewind bundled react and react-native conflicts

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,22 @@
-const { getDefaultConfig } = require("expo/metro-config");
+const path = require('path');
+const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
- 
-const config = getDefaultConfig(__dirname)
- 
-module.exports = withNativeWind(config, { input: './global.css' })
+
+const config = getDefaultConfig(__dirname);
+
+config.resolver.extraNodeModules = {
+  'react-native-css-interop': path.resolve(
+    __dirname,
+    'node_modules/nativewind/node_modules/react-native-css-interop'
+  ),
+};
+
+const nativewindModules = path.resolve(__dirname, 'node_modules/nativewind/node_modules');
+const esc = (s) => s.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+
+config.resolver.blockList = [
+  new RegExp(`^${esc(path.join(nativewindModules, 'react-native'))}(/.*)?$`),
+  new RegExp(`^${esc(path.join(nativewindModules, 'react'))}(/.*)?$`),
+];
+
+module.exports = withNativeWind(config, { input: './global.css' });
